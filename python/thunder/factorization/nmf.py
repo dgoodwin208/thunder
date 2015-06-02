81fce3809ccd754b9245e351c38e38ac3a759812
dgoodwin208 <dgoodwin208@gmail.com>
2015-06-02 18:18:35 -0400

    Added SparseNMF to the Thunder package

--- python/thunder/factorization/nmf.py
+++ python/thunder/factorization/nmf.py
@@ -215,3 +215,161 @@ class NMF(object):
             raise Exception("Algorithm %s is not supported" % self.method)
 
         return self
+
+class SparseNMF(NMF):
+    """
+        Extends NMF but with sparsification according to the
+        "Sparse non-negative matrix factorizations
+        via alternating non-negativity-constrained least squares for microarray data analysis"
+        by Kim and Park, 2012.
+        Note that their formulation is to sparsify H, but we changed it to sparsify W
+
+        Beta is by default .1
+        Eta should be by default the maximum value of the original matrix squared
+    """
+
+    def __init__(self, k=5, method='als', maxIter=20, tol=0.001, h0=None, wHist=False,
+                 reconHist='none',beta=1.,eta=1, verbose=False):
+
+        super(SparseNMF, self).__init__(k, method, maxIter, tol, h0, wHist,
+                 reconHist, verbose)
+        self.beta = beta
+        self.eta = eta
+
+    def fit(self, mat):
+        """
+        Calcuate the non-negative matrix decomposition.
+
+        Parameters
+        ----------
+        mat : Series or a subclass (e.g. RowMatrix)
+            Data to estimate independent components from, must be a collection of
+            key-value pairs where the keys are identifiers and the values are
+            one-dimensional arrays
+
+        Returns
+        ----------
+        self : returns an instance of self.
+        """
+
+        from numpy import add, any, diag, dot, inf, maximum, outer, sqrt, apply_along_axis
+        from numpy import vstack,hstack,eye,zeros, ones
+        from numpy.linalg import inv, norm, pinv, cond
+        from numpy.random import rand
+
+        if not (isinstance(mat, Series)):
+            raise Exception('Input must be Series or a subclass (e.g. RowMatrix)')
+
+        mat = mat.rdd
+
+        # a helper function to take the Frobenius norm of two zippable RDDs
+        def rddFrobeniusNorm(A, B):
+            return sqrt(A.zip(B).map(lambda ((keyA, x), (keyB, y)): sum((x - y) ** 2)).reduce(add))
+
+        # input checking
+        k = self.k
+        if k < 1:
+            raise ValueError("Supplied k must be greater than 1.")
+        m = mat.values().first().size
+        if self.h0 is not None:
+            if any(self.h0 < 0):
+                raise ValueError("Supplied h0 contains negative entries.")
+
+        # alternating least-squares implementation
+        if self.method == "als":
+
+            # initialize NMF and begin als algorithm
+            if self.verbose:
+                print "Initializing NMF"
+            alsIter = 0
+            hConvCurr = 100
+
+            if self.h0 is None:
+                # noinspection PyUnresolvedReferences
+                print "Intializing h0 to be random, scaled by .1*sqrt(eta)"
+                self.h0 = rand(k, m)*sqrt(self.eta)/10
+
+            h = self.h0
+            w = None
+
+            # goal is to solve R = WH subject to all entries of W,H >= 0
+            # by iteratively updating W and H with least squares and clipping negative values
+            while (alsIter < self.maxIter) and (hConvCurr > self.tol):
+                # update values on iteration
+                print "starting"
+                hOld = h
+                wOld = w
+
+                #Calculate the pseudoinverse with the beta weight appended
+                pinvH = pinv(vstack((h.T,sqrt(self.beta)*ones((1, self.k)))))
+                #Truncate the last colmn of the pinv before multiplying with mat
+                #See paper in the class comments for details
+                pinvH_trunc = pinvH[:, :-1]
+                # update W using least squares row-wise with R * pinv(H); then clip negative values to 0
+                w = mat.mapValues(lambda x: dot(x, pinvH_trunc.T))
+
+                # clip negative values of W
+                # noinspection PyUnresolvedReferences
+                w = w.mapValues(lambda x: maximum(x, 0))
+
+                # precompute inv(W' * W) to get inv_gramian_w, a np array
+                # We have chosen k to be small, i.e., rank(W) = k, so W'*W is invertible
+                gramianW = w.values().map(lambda x: outer(x, x)).reduce(add)
+                mod_gramianW = gramianW + self.eta*eye(self.k)
+                invGramianW = inv(mod_gramianW)
+
+                # pseudoinverse of W is inv(W' * W) * W' = inv_gramian_w * w
+                pinvW = w.mapValues(lambda x: dot(invGramianW, x))
+
+                # update H using least squares row-wise with inv(W' * W) * W * R (same as pinv(W) * R)
+                h = pinvW.values().zip(mat.values()).map(lambda (x, y): outer(x, y)).reduce(add)
+
+                # clip negative values of H
+                # noinspection PyUnresolvedReferences
+                h = maximum(h, 0)
+
+                # Because of the regularization there is no need for the normalization step
+                # h = dot(diag(1 / maximum(apply_along_axis(norm, 1, h), 0.001)), h)
+
+                # estimate convergence
+                hConvCurr = norm(h-hOld)
+                self.hConvergence.append(hConvCurr)
+                if self.wConvergence is not None:
+                    if wOld is not None:
+                        self.wConvergence.append(rddFrobeniusNorm(w, wOld))
+                    else:
+                        self.wConvergence.append(inf)
+
+                # calculate reconstruction error
+                if self.reconHist == 'all':
+                    recData = w.mapValues(lambda x: dot(x, h))
+                    self.reconErr.append(rddFrobeniusNorm(mat, recData))
+
+                # report progress
+                if self.verbose:
+                    print "finished als iteration %d with convergence = %.6f in H" % (alsIter, hConvCurr)
+
+                # increment count
+                alsIter += 1
+
+            # report on convergence
+            if self.verbose:
+                if hConvCurr <= self.tol:
+                    print "Converged to specified tolerance."
+                else:
+                    print "Warning: reached maxiter without converging to specified tolerance."
+
+            # calculate reconstruction error
+            if self.reconHist == 'final':
+                recData = w.mapValues(lambda x: dot(x, h))
+                self.reconErr = rddFrobeniusNorm(mat, recData)
+
+            # report results
+            self.h = h
+            # TODO: need to propagate metadata through to this new Series object
+            self.w = Series(w)
+
+        else:
+            raise Exception("Algorithm %s is not supported" % self.method)
+
+        return self
